338df495afb1030abe641c660cd4939edc61ad7e
Mehdi Rad <mahdirad0@gmail.com>
2022-01-08 15:22:23 +0330

    Add prisma and connect to PlanetScale

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.upsert({
+    where: { email: "ex@mple.com" },
+    update: {},
+    create: {
+      email: "ex@mple.com",
+      name: "Ex Ample",
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
